b8f87e35fccd8425f3cbbfe730697b9e8b97c171
RoloMaster23 <92298959+Rohan-Burman@users.noreply.github.com>
2024-03-03 20:32:57 +0000

    adding location

--- src/weather.js
+++ src/weather.js
@@ -15,6 +15,7 @@ const WeatherIcon = ({ icon }) => {
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [location, setLocation] = useState('London'); // Default location
+  const [timezone, setTimezone] = useState(null); // Add timezone state
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,13 +32,30 @@ const Weather = () => {
     fetchData();
   }, [location]); // Re-fetch data when location changes
 
+  useEffect(() => {
+    const fetchTimezone = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6917cc17b50462f572aaf55fd5ab64d1`
+        );
+        console.log("Timezone response:", response); // Log response to check if timezone data is fetched
+        setTimezone(response.data.timezone);
+      } catch (error) {
+        console.error("Error fetching timezone:", error);
+      }
+    };
+
+    fetchTimezone();
+  }, [location]);
+
   if (!weatherData) {
     return <div>Loading...</div>;
   }
 
   const currentHour = new Date().getHours();
   const next4HoursData = weatherData.list.filter((hour) => {
-    const hourOfForecast = moment.unix(hour.dt).tz(`Europe/${location}`).hour(); // Adjust the timezone based on location
+    if (!hour) return false; // Check if hour is undefined or null
+    const hourOfForecast = moment.unix(hour.dt).tz(`Europe/${timezone}`).hour(); // Adjust the timezone based on location
     return hourOfForecast >= currentHour && hourOfForecast < currentHour + 4;
   });
 
@@ -47,13 +65,13 @@ const Weather = () => {
 
   return (
     <div>
-      <h1>Hourly Weather Forecast for the Next 4 Hours</h1>
+      <h1 style={{ textTransform: 'capitalize' }}>{location} Hourly Weather Forecast for the Next 4 Hours</h1>
       <input type="text" value={location} onChange={handleLocationChange} />
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         {next4HoursData.map((hour, index) => (
           <div key={index} style={{ display: 'inline-block', width: '24%', margin: '1%'}}>
-            <div className="WeatherBlock"> {/* Use the WeatherBlock class */}
-              <p style={{ margin: 0 }}>{moment.unix(hour.dt).tz(`Europe/${location}`).format('HH:mm')}</p>
+            <div className="WeatherBlock"> {}
+              <p style={{ margin: 0 }}>{moment.unix(hour.dt).tz(`Europe/${timezone}`).format('HH:mm')}</p>
               <WeatherIcon icon={hour.weather[0].icon} />
               <p style={{ margin: 0 }}>{Math.round(hour.main.temp)}°C</p>
             </div>
