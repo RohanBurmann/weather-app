3cc2c8c670191d03abf853cfa767e798fcbcc7a1
RoloMaster23 <92298959+Rohan-Burman@users.noreply.github.com>
2024-03-03 19:53:15 +0000

    inital commits

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import moment from 'moment-timezone';
+import './WeatherBlock.css'; // Import the external stylesheet
+
+const WeatherIcon = ({ icon }) => {
+  const baseUrl = 'http://openweathermap.org/img/wn/';
+  const size = '@2x';
+  const extension = '.png';
+  const iconUrl = `${baseUrl}${icon}${size}${extension}`;
+
+  return <img src={iconUrl} alt="Weather Icon" />;
+};
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -8,10 +19,11 @@ const Weather = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'https://api.openweathermap.org/data/2.5/weather?q=London&appid=6917cc17b50462f572aaf55fd5ab64d1&units=metric');
+          "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=6917cc17b50462f572aaf55fd5ab64d1&units=metric"
+        );
         setWeatherData(response.data);
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error("Error fetching weather data:", error);
       }
     };
 
@@ -22,13 +34,26 @@ const Weather = () => {
     return <div>Loading...</div>;
   }
 
+  const currentHour = new Date().getHours();
+  const next4HoursData = weatherData.list.filter((hour) => {
+    const hourOfForecast = moment.unix(hour.dt).tz('Europe/London').hour(); // Adjust the timezone as needed
+    return hourOfForecast >= currentHour && hourOfForecast < currentHour + 4;
+  });
+
   return (
     <div>
-      <h1>Weather Forecast</h1>
-      <p>Temperature: {weatherData.main.temp}°C</p>
-      <p>Feels Like: {weatherData.main.feels_like}°C</p>
-      <p>Humidity: {weatherData.main.humidity}%</p>
-      <p>Weather: {weatherData.weather[0].description}</p>
+      <h1>Hourly Weather Forecast for the Next 4 Hours</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {next4HoursData.map((hour, index) => (
+          <div key={index} style={{ display: 'inline-block', width: '24%', margin: '1%'}}>
+            <div className="WeatherBlock"> {/* Use the WeatherBlock class */}
+              <p style={{ margin: 0 }}>{moment.unix(hour.dt).tz('Europe/London').format('HH:mm')}</p>
+              <WeatherIcon icon={hour.weather[0].icon} />
+              <p style={{ margin: 0 }}>{Math.round(hour.main.temp)}°C</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
